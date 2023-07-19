d5e6496d457b9d43a26825862d22a1587ae6c364
Nez <nezzengame@gmail.com>
2023-07-19 21:48:47 +0200

    HR-PS-task-CompareTheTriplets:https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true-clearerNotation

--- src/main/java/task2/CompareTheTriplets.java
+++ src/main/java/task2/CompareTheTriplets.java
@@ -1,21 +1,23 @@
 package task2;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.Integer.compare;
-import static java.util.Arrays.asList;
 
 public class CompareTheTriplets {
+    public static final int ALICE_WIN = 1;
+    public static final int BOB_WIN = (-1);
+
     public List<Integer> compareTriplets(List<Integer> pointsA, List<Integer> pointsB) {
-        List<Integer> pointsAwardResult = asList(0, 0);
+        int aliceScore = 0;
+        int bobScore = 0;
+
         for (int i = 0; i < 3; i++) {
-            int pointsAwardByCategories = compare(pointsA.get(i), pointsB.get(i));
-            if (pointsAwardByCategories == 1) {
-                pointsAwardResult.set(0, pointsAwardResult.get(0) + 1);
-            } else if (pointsAwardByCategories == (-1)) {
-                pointsAwardResult.set(1, pointsAwardResult.get(1) + 1);
-            }
+            int comparisonResult = compare(pointsA.get(i), pointsB.get(i));
+            aliceScore += comparisonResult == ALICE_WIN ? 1 : 0;
+            bobScore += comparisonResult == BOB_WIN ? 1 : 0;
         }
-        return pointsAwardResult;
+        return Arrays.asList(aliceScore, bobScore);
     }
 }

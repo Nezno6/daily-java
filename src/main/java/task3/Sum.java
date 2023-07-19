ad6fc297838cba3843fa1091f563733d5fe2dd92
Nez <nezzengame@gmail.com>
2023-07-19 23:56:34 +0200

    HR-PS-task-3-SolveMeFirst:https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true

---
+++ src/main/java/task3/Sum.java
@@ -0,0 +1,9 @@
+// TASK: https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+package task3;
+
+public class Sum {
+    public int addition(int firstNumb, int secondNumb) {
+        return firstNumb + secondNumb;
+    }
+}

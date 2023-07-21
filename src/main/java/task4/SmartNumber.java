41c06ac9416847c153288847bfec4542a5adc069
Nez <nezzengame@gmail.com>
2023-07-21 11:51:30 +0200

    HR-PS-task-4-SmartNumber:https://www.hackerrank.com/challenges/smart-number/problem?isFullScreen=true::FIX-BUG:one-should-have-YES

--- src/main/java/task4/SmartNumber.java
+++ src/main/java/task4/SmartNumber.java
@@ -1,13 +1,13 @@
 package task4;
-
 public class SmartNumber {
-    public String isSmartNumber(int number) {
-        int count = 1;
-        for (int i = 2; i <= number; i++) {
-            if (number%i==0){
-                count++;
+
+        public String isSmartNumber(int number) {
+            int count = 1;
+            for (int i = 2; i <= number; i++) {
+                if (number%i==0){
+                    count++;
+                }
             }
+            return (count%2==0)?"NO":"YES";
         }
-        return ((count%2==0)||(count<=1))?"NO":"YES";
     }
-}
--- src/test/java/task4/SmartNumberTest.java
+++ src/test/java/task4/SmartNumberTest.java
@@ -13,11 +13,14 @@ public class SmartNumberTest {
 
     static Stream<Arguments> numbersToCheckIsSmart() {
         return Stream.of(
-                Arguments.of(4, "YES"),
-                Arguments.of(1, "NO"),
-                Arguments.of(2, "NO"),
-                Arguments.of(7, "NO"),
-                Arguments.of(169, "YES")
+                Arguments.of(4, "YES"), // 1,2,4
+                Arguments.of(1, "YES"), // 1
+                Arguments.of(2, "NO"), // 1,2
+                Arguments.of(7, "NO"), // 1,7
+                Arguments.of(49, "YES"), // 1,7,49
+                Arguments.of(74, "NO"), // 1,2,37,74
+                Arguments.of(169, "YES"), // 1, 13, 169
+                Arguments.of(2401, "YES") // 1, 7, 49, 343, 2401
         );
     }
 

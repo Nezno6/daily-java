8ea91339ba6299c00686967f6146ec91687fbc57
Nez <nezzengame@gmail.com>
2023-07-21 12:51:41 +0200

    HR-PS-task-4-SmartNumber::BETTER:reducing-the-divisor-search-range-by-half::https://www.hackerrank.com/challenges/smart-number/problem?isFullScreen=true

--- src/main/java/task4/SmartNumber.java
+++ src/main/java/task4/SmartNumber.java
@@ -1,9 +1,10 @@
 package task4;
+
 public class SmartNumber {
 
         public String isSmartNumber(int number) {
             int count = 1;
-            for (int i = 2; i <= number; i++) {
+            for (int i = 1; i <= number/2; i++) {
                 if (number%i==0){
                     count++;
                 }

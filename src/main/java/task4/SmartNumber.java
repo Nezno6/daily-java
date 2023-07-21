7dbcdc4b753dc27f42f4efcccf25a76b3ea34cca
Nez <nezzengame@gmail.com>
2023-07-21 19:58:12 +0200

    HR-PS-task-4-SmartNumber::BETTER:accordance-with-the-guidelines::https://www.hackerrank.com/challenges/smart-number/problem?isFullScreen=true

--- src/main/java/task4/SmartNumber.java
+++ src/main/java/task4/SmartNumber.java
@@ -1,18 +1,12 @@
 package task4;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class SmartNumber {
 
-        public String isSmartNumber(int number) {
-            Set<Integer> divisors = new HashSet<>();
-            for (int i = 1; i <= Math.sqrt(number); i++) {
-                if (number%i==0){
-                    divisors.add(i);
-                    divisors.add(number/i);
-                }
-            }
-            return divisors.size()%2==1?"YES":"NO";
+    public String isSmartNumber(int number) {
+        int numberSqrt = (int) Math.sqrt(number);
+        if (numberSqrt * numberSqrt == number) {
+            return "YES";
         }
+        return "NO";
     }
+}
--- src/test/java/task4/SmartNumberTest.java
+++ src/test/java/task4/SmartNumberTest.java
@@ -12,15 +12,17 @@ public class SmartNumberTest {
     SmartNumber smartNumber = new SmartNumber();
 
     static Stream<Arguments> numbersToCheckIsSmart() {
-        return Stream.of(
-                Arguments.of(4, "YES"), // 1,2,4
+        return Stream.of(Arguments.of(4, "YES"), // 1,2,4
                 Arguments.of(1, "YES"), // 1
                 Arguments.of(2, "NO"), // 1,2
                 Arguments.of(7, "NO"), // 1,7
                 Arguments.of(49, "YES"), // 1,7,49
+                Arguments.of(64, "YES"), // 1, 2, 4, 8, 16, 32, 64
+                Arguments.of(65, "NO"), // 1, 5, 13, 65
                 Arguments.of(74, "NO"), // 1,2,37,74
                 Arguments.of(169, "YES"), // 1, 13, 169
-                Arguments.of(2401, "YES") // 1, 7, 49, 343, 2401
+                Arguments.of(2401, "YES"), // 1, 7, 49, 343, 2401
+                Arguments.of(14641, "YES") // 1, 11, 121, 1331, 14641
         );
     }
 

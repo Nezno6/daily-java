4974eb2b8b0581808943a4023717d27fc1a8cc9e
Nez <nezzengame@gmail.com>
2023-08-04 21:40:10 +0200

    HR-PS-task-13-BetweenTwoSets::REFACTOR:Improved-performance-by-calling-only-one-method-in-a-loop-and-applying-multiples-in-the-count-increment::https://www.hackerrank.com/challenges/between-two-sets/problem?isFullScreen=true

--- src/main/java/task13/BetweenTwoSets.java
+++ src/main/java/task13/BetweenTwoSets.java
@@ -2,30 +2,20 @@
 package task13;
 
 import java.util.List;
-import java.util.OptionalInt;
 
 public class BetweenTwoSets {
     // element%d1=0 and element%d2=0 and element%d3=0 --> element >= max(d1,d2,d3) --> element is a multiple
     // m1%element=0 and m2%element=0 and m3%element=0 --> element <= min(m1,m2,m3) --> element is a divisor
     public int getSizeOfWantedSet(List<Integer> divisors, List<Integer> multiples) {
         int counter = 0;
-        int maxDivisor = max(divisors);
-        int minMultiple = min(multiples);
-        for (int i = maxDivisor; i <= minMultiple; i++) {
-            if (isMultipleOfAll(divisors, i) && isDivisorOfAll(multiples, i)) {
+        int lowerBoundFromLcmSet = lcm(divisors);
+        int upperBoundFromGcdSet = gcd(multiples);
+        for (int i = lowerBoundFromLcmSet; i <= upperBoundFromGcdSet; i += lowerBoundFromLcmSet) {
+            if (isDivisorOfAll(multiples, i)) {
                 counter++;
             }
         }
         return counter;
-    }
-
-    private boolean isMultipleOfAll(List<Integer> divisors, int i) {
-        for (Integer divisor : divisors) {
-            if (i % divisor != 0) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private boolean isDivisorOfAll(List<Integer> multiples, int i) {
@@ -37,19 +27,28 @@ public class BetweenTwoSets {
         return true;
     }
 
-    private int max(List<Integer> divisors) {
-        OptionalInt max = divisors.stream().mapToInt(e -> e).max();
-        if (max.isPresent()) {
-            return max.getAsInt();
-        }
-        throw new IllegalArgumentException("Max method accepts only numbers");
+    int gcd(int firstNum, int secondNum) {
+        if (secondNum == 0) return firstNum;
+        else return gcd(secondNum, firstNum % secondNum);
     }
 
-    private int min(List<Integer> multiples) {
-        OptionalInt min = multiples.stream().mapToInt(e -> e).min();
-        if (min.isPresent()) {
-            return min.getAsInt();
+    int gcd(List<Integer> numbers) {
+        int result = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            result = gcd(numbers.get(i), result);
         }
-        throw new IllegalArgumentException("Min method accepts only numbers");
+        return result;
+    }
+
+    int lcm(int firstNum, int secondNum) {
+        return Math.abs(firstNum * secondNum) / gcd(firstNum, secondNum);
+    }
+
+    int lcm(List<Integer> numbers) {
+        int result = numbers.get(0);
+        for (int i = 1; i < numbers.size(); i++) {
+            result = lcm(numbers.get(i), result);
+        }
+        return result;
     }
 }
--- src/test/java/task13/BetweenTwoSetsTest.java
+++ src/test/java/task13/BetweenTwoSetsTest.java
@@ -5,22 +5,43 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class BetweenTwoSetsTest {
     BetweenTwoSets betweenTwoSets = new BetweenTwoSets();
 
-    public static Stream<Arguments> dataForGetSizeOfSearchedSet() {
-        return Stream.of(
-                Arguments.of(List.of(2, 6), List.of(24, 36), 2),
-                Arguments.of(List.of(2, 4), List.of(16, 32, 96), 3));
+    public static Stream<Arguments> dataForV2GetSizeOfSearchedSet() {
+        return Stream.of(Arguments.of(List.of(2, 6, 3), List.of(24 * 10_000_000, 36 * 10_000_000, 12 * 100_000_000, 48 * 10_000_000), 72), Arguments.of(List.of(2, 4), List.of(16, 32, 96), 3));
+    }
+
+    public static Stream<Arguments> dataForGCDList() {
+        return Stream.of(Arguments.of(Arrays.asList(2, 6, 8), 2), Arguments.of(Arrays.asList(2, 4), 2), Arguments.of(Arrays.asList(12, 18, 24), 6), Arguments.of(Arrays.asList(15, 25, 35), 5), Arguments.of(Arrays.asList(20, 40, 60), 20), Arguments.of(Arrays.asList(20, 1, 60), 1), Arguments.of(Arrays.asList(8, 7, 12), 1));
+    }
+
+    public static Stream<Arguments> dataForLCMList() {
+        return Stream.of(Arguments.of(Arrays.asList(15, 20), 60), Arguments.of(Arrays.asList(12, 18), 36), Arguments.of(Arrays.asList(21, 28), 84), Arguments.of(Arrays.asList(1, 28), 28), Arguments.of(Arrays.asList(0, 1), 0), Arguments.of(Arrays.asList(0, 8), 0));
     }
 
     @ParameterizedTest
-    @MethodSource("dataForGetSizeOfSearchedSet")
-    void getSizeOfWantedSetTest(List<Integer> divisors, List<Integer> multiples, int expected) {
+    @MethodSource("dataForV2GetSizeOfSearchedSet")
+    void getSizeOfWantedSetV2Test(List<Integer> divisors, List<Integer> multiples, int expected) {
         int actual = betweenTwoSets.getSizeOfWantedSet(divisors, multiples);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataForGCDList")
+    void getGCDTest(List<Integer> numbers, int expected) {
+        int actual = betweenTwoSets.gcd(numbers);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataForLCMList")
+    void getLCMTest(List<Integer> numbers, int expected) {
+        int actual = betweenTwoSets.lcm(numbers);
         Assertions.assertEquals(expected, actual);
     }
 }

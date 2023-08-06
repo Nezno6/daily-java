a36f5435baf1ef8591f8957a257055be092c5185
Nez <nezzengame@gmail.com>
2023-08-06 17:59:00 +0200

    HR-PS-task-14-ps-SherlockAndArray::ANOTHER-SOLUTION:Used-recursion-but-time-complexity-increased-to-n-log-n-and-space-complexity-is-log-n::https://www.hackerrank.com/challenges/sherlock-and-array/problem

--- src/main/java/task14/SherlockAndArray.java
+++ src/main/java/task14/SherlockAndArray.java
@@ -8,14 +8,27 @@ public class SherlockAndArray {
 
 
     public boolean balancedSums(List<Integer> numbers) {
-        int sumOfAllList = numbers.stream().reduce(Integer::sum).orElse(0);
-        int sum = 0;
-        for (Integer number : numbers) {
-            if ((2 * sum + number) == sumOfAllList) {
-                return true;
-            }
-            sum += number;
+        return balancedSumsHelper(numbers, 0, numbers.size() - 1);
+    }
+
+    private boolean balancedSumsHelper(List<Integer> numbers, int start, int end) {
+
+        if (start > end) {
+            return false;
         }
-        return false;
+
+        int middle = (start + end) / 2;
+        int sumToMiddle = numbers.subList(0, middle).stream().reduce(Integer::sum).orElse(0);
+        int sumFromMiddleUpOne = numbers.subList(middle + 1, numbers.size()).stream().reduce(Integer::sum).orElse(0);
+
+        if (sumToMiddle == sumFromMiddleUpOne) {
+            return true;
+        }
+
+        if (sumToMiddle > sumFromMiddleUpOne) {
+            return balancedSumsHelper(numbers, start, middle - 1);
+        }
+
+        return balancedSumsHelper(numbers, middle + 1, end);
     }
 }
